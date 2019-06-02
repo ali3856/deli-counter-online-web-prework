b87ce7a0ffe296ff31361390d4ee3ee1e86026df
witty-banner-1950 <asimsali95@gmail.com>
2019-06-02 19:16:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,6 @@
 # Write your code here.
+katz_deli = []
+
 def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
@@ -14,7 +16,6 @@ end
 
 def line(katz_deli)
   print "The line is currently empty:"
-
     katz_deli.each_with_index do |person, index|
       message += "#{index.to_i+1}. #{person}"
     end
